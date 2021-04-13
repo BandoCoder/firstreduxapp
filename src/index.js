f692e91cd16bcdd1527c6463fdbb0353e4550492
kittybagpipes <sparticus978@gmail.com>
2021-04-13 06:12:48 -0700

    built store, and reducer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+import store from "./store";
+
+const unsubscribe = store.subscribe(() => {
+  console.log("store changed!", store.getState());
+});
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1",
+  },
+});
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1,
+  },
+});
+console.log(store.getState());
