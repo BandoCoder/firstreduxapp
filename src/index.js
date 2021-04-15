2a0c36ecb98448cab73dcba742154240558c0d88
kittybagpipes <sparticus978@gmail.com>
2021-04-15 07:10:55 -0700

    actions updated

--- src/index.js
+++ src/index.js
@@ -1,23 +1,14 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded, bugRemoved, bugResolved } from "./actions";
 
 const unsubscribe = store.subscribe(() => {
   console.log("store changed!", store.getState());
 });
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1",
-  },
-});
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
 
-unsubscribe();
+// unsubscribe();
 
-store.dispatch({
-  type: actions.BUG_REMOVED,
-  payload: {
-    id: 1,
-  },
-});
+// store.dispatch(bugRemoved(1));
 console.log(store.getState());
